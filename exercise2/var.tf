71a7c62ac29b46825385492b2b5973e8e1133876
bhargavteja1999 <143714131+bhargavteja1999@users.noreply.github.com>
2024-09-21 14:40:19 +0530

    Update var.tf

--- exercise2/var.tf
+++ exercise2/var.tf
@@ -24,5 +24,8 @@ variable "AMIS" {
   type = map(any)
   default = {
     us-east-1 = "ami-0e86e20dae9224db8"
+    us-east-2 = "ami-085f9c64a9b75eed5"
+    us-west-1 = "ami-0d53d72369335a9d6"
+    us-west-2 = "ami-08d8ac128e0a1b91c"
   }
 }
